83d412fe1fb34094f4152855d803300fb515f8e1
jishnujayakumar <jishnu@cs260569.utdallas.edu>
2025-06-24 17:56:41 -0500

    + docker

--- robokit/docker/run_container.sh
+++ robokit/docker/run_container.sh
@@ -1,8 +1,3 @@
-# Source: https://github.com/NVlabs/BundleSDF/blob/master/docker/run_container.sh
-# The BundleSDF docker image worked without the error of cuda mismatch after package installation. Hence using it.
-# This will open the root folder so be careful and navigate to the project folder before dev
-
-container_name="robokit"
-docker rm -f $container_name
+docker rm -f rpx
 DIR=$(pwd)/../
-xhost +  && docker run --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -it --network=host --name $container_name  --cap-add=SYS_PTRACE --security-opt seccomp=unconfined  -v /home:/home -v /tmp:/tmp -v /mnt:/mnt -v $DIR:$DIR  --ipc=host -e DISPLAY=${DISPLAY} -e GIT_INDEX_FILE irvlutd/bundle-sdf:latest bash
+xhost +local:docker  && docker run --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -itd --network=host --name rpx  --cap-add=SYS_PTRACE --security-opt seccomp=unconfined  -v /home:/home -v /tmp:/tmp -v /mnt:/mnt -v $DIR:$DIR  --ipc=host -e DISPLAY=${DISPLAY} -e GIT_INDEX_FILE -e ROS_MASTER_URI=$1 ROS_HOSTNAME=$2 irvlutd/hrt1-rpx:latest bash
